1bd6528ea2ff99a195f82c5b9f78a260deb79db8
nova-1001 <nova4710@outlook.jp>
2022-11-24 22:12:02 +0900

    Paietteを作りました

--- src/views/PaletteApp.vue
+++ src/views/PaletteApp.vue
@@ -9,6 +9,43 @@
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      red: 0,
+      green: 0,
+      colors: [
+        // { red: 0, green: 0 }
+      ],
+    }
+  },
+  methods: {
+    changeColor(e) {
+      this.red = e.offsetX
+      this.green = e.offsetY
+    },
+    pickColor() {
+      const newColor = {
+        red: this.red,
+        green: this.green,
+      }
+      this.colors.push(newColor)
+    },
+    showColor(color) {
+      this.red = color.red
+      this.green = color.green
+    },
+  },
+  computed: {
+    paletteStyle() {
+      return {
+        backgroundColor: `rgba(${this.red}, ${this.green}, 200, 0.5)`,
+      }
+    },
+  },
+}
+</script>
 <style>
 .app {
   display: flex;
